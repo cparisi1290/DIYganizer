5b72913ebe386c494726b7e640b20f26bafde9c0
Cassandra <cparisi1290@gmail.com>
2021-03-15 11:10:45 -0400

    create user resources, set up omniauth for google, create root welcome page to login/signup, create user show page once logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  root to: 'sessions#welcome'
+
+  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 end
