5b3998dc6882c6a7d381c7e7bc070f5ad9d499d0
Cassandra <cparisi1290@gmail.com>
2021-03-15 14:10:11 -0400

    set up project create action to render project show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,25 @@ class ProjectsController < ApplicationController
     def new
         @project = Project.new
     end
+
+    def create
+        @project = Project.new(project_params)
+        if @project.save
+            redirect_to project_path(@project)
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(
+            :name, 
+            :status, 
+            :est_date_to_complete, 
+            :notes, 
+            :user_id, 
+            :room_id)
+    end
 end
