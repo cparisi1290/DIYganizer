3533ee9d4bd3dc6f488806f58ea12b4e77b89505
Cassandra <cparisi1290@gmail.com>
2021-03-15 12:39:48 -0400

    add create actions for users and set user params method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,18 @@ class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :new
+        end
+    end
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
