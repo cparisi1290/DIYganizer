ad41ab0c896344e7245e04b9f2ccc3bc35325f04
Cassandra <cparisi1290@gmail.com>
2021-03-15 16:32:48 -0400

    generate resources for rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_174506) do
+ActiveRecord::Schema.define(version: 2021_03_15_202032) do
 
   create_table "projects", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2021_03_15_174506) do
     t.text "notes"
     t.integer "user_id"
     t.integer "room_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
