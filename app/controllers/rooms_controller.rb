3f2b047be62e3ecc58d98da59efba44629ee6cfc
Cassandra <cparisi1290@gmail.com>
2021-03-16 12:02:04 -0400

    build nested form in rooms for projects

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -8,5 +8,28 @@ class RoomsController < ApplicationController
         @room = Room.find(params[:id])
     end
 
-    
+    def new
+        @room = Room.new
+        @room.projects.build
+    end
+
+    def create
+        @room = Room.new(room_params)
+        if @room.save 
+            redirect_to room_path(@room)
+        else
+            render :new
+        end
+    end
+
+    def room_params
+        params.require(:room).permit(
+            :name, 
+            :project_id, 
+            project_attributes: 
+                [:name, 
+                :status, 
+                :est_date_to_complete, 
+                :notes, ])
+    end
 end
