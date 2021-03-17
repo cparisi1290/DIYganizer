1a4befa1ad4494464594e70e8a3fe53e1957f53c
Cassandra <cparisi1290@gmail.com>
2021-03-17 10:24:58 -0400

    assocaitions and validations

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,6 +2,6 @@ class Room < ApplicationRecord
     has_many :projects
 
     accepts_nested_attributes_for :projects, reject_if: proc { |attributes| attributes['name'].blank? || attributes['status'].blank? || attributes['est_date_to_complete'].blank? || attributes['notes'].blank? }
-    
+    validates_presence_and_uniquness_of :name
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
     has_secure_password
 
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
+    validates :email, presence: true, uniqueness: true
+
+    has_many :projects
+    has_many :rooms, through: :projects
+
+
     def self.from_omniauth(response)
         User.find_or_create_by(uid: response[:id], provider: response[:provider]) do |u|
             u.name = response[:info][:name]
