aac5eff1205f9ea4df9437a63de87ab0571ad568
Cassandra <cparisi1290@gmail.com>
2021-03-22 15:43:36 -0400

    add helper methods to refactor logic from views

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,34 @@
 module ProjectsHelper
+
+    def display_index_header
+        if @room
+             content_tag(:h1, "#{@room.name}  Projects")
+        else
+             content_tag(:h1, "All Projects")
+        end
+   end
+
+   def display_new_header
+        if @room
+            content_tag(:h1, "Create a #{@room.name} Project")
+        else
+            content_tag(:h1, "Create a Project")
+        end
+    end
+
+    def room_form_field(f)
+        if @room
+             hidden_field :room_id, value: @room.id
+        else
+             render partial: "room_fields", locals: { f: f }
+        end
+    end
+
+    # def new_project
+    #     if params[:room_id] && @room = Room.find(params[:room_id])
+    #         content_tag(:h3, link_to "Create a New Project", new_room_project_path)
+    #     else
+    #         content_tag(:h3, link_to "Create a New Project", new_project_path)
+    #     end
+    # end
 end
