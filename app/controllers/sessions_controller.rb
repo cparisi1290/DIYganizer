a78e47354b88897f52f9cce52fe679d4d55edefb
Cassandra <cparisi1290@gmail.com>
2021-03-22 15:39:38 -0400

    add create method for user and check to see if inputs are valid

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,6 +12,17 @@ class SessionsController < ApplicationController
         redirect_to '/'
     end
 
+    def create
+        user = User.find_by_email(params[:user][:email])
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            flash[:message] = "Invalid credentials. Please try again."
+            redirect_to '/signup'
+        end
+    end
+
     def omniauth
         user = User.from_omniauth(request.env['omniauth.auth'])
         if user.valid?
